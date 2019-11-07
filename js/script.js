4b3ef4e7a8dc01a00800cee11e8bac89301d1740
Kirsten McCain <kirstenmccain@outlook.com>
2019-11-07 00:27:53 -0500

    add css, html, js files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#hidden").hover(function(){
+        $(this).css("color", "black");
+    },
+    function(){
+        $(this).hide();
+    });
+
+    $("button").click(function() {
+        alert("I am alerting you");
+    });
+});
