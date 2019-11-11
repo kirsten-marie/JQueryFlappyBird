d3f4816d7ebd307680a107a58e02d5f720f8ecdb
Kirsten McCain <kirstenmccain@outlook.com>
2019-11-11 14:05:57 -0500

    add collision for poles

--- game.js
+++ game.js
@@ -25,32 +25,43 @@ $(document).ready(function() {
 
     var the_game = setInterval(function() {
 
-        var pole_current_position = parseInt(pole.css('right'));
+        if(collision(bird, pole_1) || collision(bird, pole_2)){
+            stopGame();
 
-        //check if the poles went out of the container
-        if (pole_current_position > container_width){
-            var new_height = parseInt(Math.random() * 100);
+        } else {
 
-            //change the height before allowing pole to reappear
-            pole_1.css('height', pole_initial_height + new_height);
-            pole_2.css('height', pole_initial_height - new_height);
+            var pole_current_position = parseInt(pole.css('right'));
 
-            //increase speed
-            speed += 1;
-            speed_span.text(speed);
+            //check if the poles went out of the container
+            if (pole_current_position > container_width){
+                var new_height = parseInt(Math.random() * 100);
 
-            //set back to original position
-            pole_current_position = pole_initial_position;
-        }
+                //change the height before allowing pole to reappear
+                pole_1.css('height', pole_initial_height + new_height);
+                pole_2.css('height', pole_initial_height - new_height);
 
-        //move the pole to the right
-        pole.css('right', pole_current_position + speed);
+                //increase speed
+                speed += 1;
+                speed_span.text(speed);
 
-        if (!go_up){
-            go_down();
+                //set back to original position
+                pole_current_position = pole_initial_position;
+            }
+
+            //move the pole to the right
+            pole.css('right', pole_current_position + speed);
+
+            if (!go_up){
+                go_down();
+            }
         }
 
     }, 40);
+
+    function stopGame(){
+        clearInterval(the_game);
+        restart_button.css("display", "inline-block");
+    }
 
     $(document).on('keydown', function(e){
         var key = e.keyCode;
@@ -73,6 +84,24 @@ $(document).ready(function() {
 
     function up(){
         bird.css('top', parseInt(bird.css('top')) -5);
+    }
+
+    function collision($div1, $div2){
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+        return true;
     }
 
 });
