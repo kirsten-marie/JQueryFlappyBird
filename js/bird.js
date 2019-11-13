188556f6b982eeb2ac6c94220fd22e7518c470b3
Kirsten McCain <49414430+kirsten-marie@users.noreply.github.com>
2019-11-12 23:34:37 -0500

    add score to canvas

--- js/bird.js
+++ js/bird.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-    //constants
+    //Set game constants
     const ctx = document.getElementById("myCanvas").getContext("2d");
-    
+    const spriteWidth = 200;
+    const spriteHeight = 139;
+
+    //Create image for the sprite sheet & set the source
+    //Event listener ensures that image has loaded or it can't be drawn
     var birdSprites = new Image();
     birdSprites.addEventListener('load', function(){
         loadStartMenu();
     }, false);
     birdSprites.src = 'img/birdspritesheet2.png';
 
-    var spriteHeight = 139;
-    var spriteWidth = 200;
-
     
     //Create variable for the bird & it's css properties (height/width)
+    //should be able to delete with canvas, also delete from css
     var bird = $('#bird');
     var birdHeight = bird.css("height");
     var birdWidth = bird.css("width");
-    
-
 
     //Number of bird images on the sprite sheet
     var numImages = 4;
@@ -25,16 +25,24 @@ $(document).ready(function() {
 
     //var animation = setInterval(moveSprite, 100);
     
-    
+    //Set Game Variables
+    var score = 0;
 
     function loadStartMenu(){
-        $('#myContainer').fadeIn("slow");
+        $('#myCanvas').fadeIn("slow");
         draw();
     }
 
     function draw(){
 
-        ctx.drawImage(birdSprites, 20, 20);
+
+        //draw 1st bird from sprite sheet and scale it down
+        ctx.drawImage(birdSprites, 0, 0, spriteWidth, spriteHeight, 20, 20, spriteWidth/4.5, spriteHeight/4.5);
+        ctx.fillStyle = "red";
+        ctx.font = "italic bold 20px 'Passion One', sans-serif";
+        ctx.fillText(score, ctx.canvas.width/2, 20, 30);
+        ctx.textAlign = 'start'; 
+        ctx.textBaseline = 'top';
 
     }
 
