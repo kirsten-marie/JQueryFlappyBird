680595d4c22656b177313842bc2a9d6ebb8ae178
Kirsten McCain <49414430+kirsten-marie@users.noreply.github.com>
2019-11-14 21:51:43 -0500

    remove unneeded stuff

--- js/bird.js
+++ js/bird.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     const spriteWidth = 200;
     const spriteHeight = 139;
 
+    //Set Game Variables
+    var score = 0;
+
     //Create image for the sprite sheet & set the source
     //Event listener ensures that image has loaded or it can't be drawn
     var birdSprites = new Image();
@@ -11,7 +14,6 @@ $(document).ready(function() {
         loadStartMenu();
     }, false);
     birdSprites.src = 'img/birdspritesheet2.png';
-
     
     //Create variable for the bird & it's css properties (height/width)
     //should be able to delete with canvas, also delete from css
@@ -22,11 +24,8 @@ $(document).ready(function() {
     //Number of bird images on the sprite sheet
     var numImages = 4;
     var cont = 0;
-
-    //var animation = setInterval(moveSprite, 100);
     
-    //Set Game Variables
-    var score = 0;
+
 
     function loadStartMenu(){
         $('#myCanvas').fadeIn("slow");
@@ -38,20 +37,27 @@ $(document).ready(function() {
 
         //draw 1st bird from sprite sheet and scale it down
         ctx.drawImage(birdSprites, 0, 0, spriteWidth, spriteHeight, 20, 20, spriteWidth/4.5, spriteHeight/4.5);
-        ctx.fillStyle = "red";
-        ctx.font = "italic bold 20px 'Passion One', sans-serif";
-        ctx.fillText(score, ctx.canvas.width/2, 20, 30);
+        ctx.fillStyle = "black";
+        ctx.font = 'bold 20px sans-serif';
+        ctx.fillText(score, ctx.canvas.width/2, 20);
         ctx.textAlign = 'start'; 
         ctx.textBaseline = 'top';
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
 
-    }
+        ctx.fillStyle = "purple";
+        ctx.fillRect(300, 0, 50, 50);
+        
+        //testing out composite assets
 
-    function moveSprite(){
-        img.css('margin-top', -1 * (cont * imgHeight));
-        cont++;
-        if (cont == numImages){
-            clearInterval(animation);
-        }
+        globalAlpha = 0.5;
+        globalCompositeOperation = 'source-atop';
+        //source-atop || source-in || source-out || source-over (default) || destination-atop || destination-in || destination-out || destination-over || lighter || copy || xor
+
+        ctx.fillStyle = "orange";
+        ctx.fillRect(300, 20, 50, 50);
     }
         
 });
