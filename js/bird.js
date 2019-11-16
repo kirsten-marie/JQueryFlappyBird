d90663a622f6b55fcbe26df88de916c111f6aa6a
Kirsten McCain <kirstenmccain@outlook.com>
2019-11-15 22:00:35 -0500

    workingonanimation

--- js/bird.js
+++ js/bird.js
@@ -4,33 +4,69 @@ $(document).ready(function() {
     const spriteWidth = 200;
     const spriteHeight = 139;
 
+    //Set canvas variables
+    var canvasHeight = ctx.canvas.height;
+    var canvasWidth = ctx.canvas.width;
     //Set Game Variables
     var score = 0;
+    gameOver = false;
 
+    /*
     //Create image for the sprite sheet & set the source
     //Event listener ensures that image has loaded or it can't be drawn
     var birdSprites = new Image();
     birdSprites.addEventListener('load', function(){
         loadStartMenu();
     }, false);
-    birdSprites.src = 'img/birdspritesheet2.png';
-    
+    birdSprites.src = 'img/birdspritesheet.png';
+
     //Create variable for the bird & it's css properties (height/width)
     //should be able to delete with canvas, also delete from css
     var bird = $('#bird');
     var birdHeight = bird.css("height");
     var birdWidth = bird.css("width");
+    */
+
+   var birdSprites = new Image();
+   birdSprites.src = 'img/birdspritesheet.png';
+   birdSprites.onload = function(){
+        startAnimation();
+   };
+    
+   function startAnimation(){
+       ctx.fillStyle = 'aliceblue';
+       ctx.fillRect(0, 0, ctx.canvasWidth, ctx.canvasHeight);
+       let sourceX = 200,
+       sourceY = 0,
+       sourceWidth = spriteWidth,
+       sourceHeight = spriteHeight,
+       destX = 50,
+       destY = 50,
+       destWidth = spriteWidth/3,
+       destHeight = spriteHeight/3;
+       ctx.drawImage(birdSprites, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+   }
 
     //Number of bird images on the sprite sheet
     var numImages = 4;
     var cont = 0;
     
-
+    var id = setInterval(frame, 5);
+    
+    function frame(){
+        if (!gameOver){
+            clearInterval(id);
+        }
+        else{
+            //animation code
+        }
+    }
 
     function loadStartMenu(){
         $('#myCanvas').fadeIn("slow");
         draw();
     }
+
 
     function draw(){
 
@@ -47,7 +83,7 @@ $(document).ready(function() {
         ctx.msImageSmoothingEnabled = false;
         ctx.imageSmoothingEnabled = false;
         
-
+/*
         ctx.fillStyle = "purple";
         ctx.fillRect(70, 20, 45, 40);
 
@@ -61,6 +97,7 @@ $(document).ready(function() {
 
         ctx.fillStyle = "orange";
         ctx.fillRect(100, 40, 50, 50);
+        */
     }
         
 });
